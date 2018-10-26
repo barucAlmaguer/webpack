96d12c171c8cb54d0384ab7c772e5476e5150485
Baruc Almaguer <baruc.almaguer@gmail.com>
2018-10-26 13:54:17 -0500

    section 3 finished

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,13 @@
+import big from '../assets/big.jpg'
+import smol from '../assets/smol.jpg'
 import './css/image_viewer.css'
 
 const image = document.createElement('img');
-image.src = 'https://valiot.io/img/eliot-about.png'
+image.src = smol;
 
 document.body.appendChild(image);
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
+
+document.body.appendChild(bigImage);
